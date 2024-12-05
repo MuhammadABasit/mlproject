94666c1acde7e86094d64c33d6cb08692411b1f1
MuhammadABasit <muhammb1@umbc.edu>
2024-12-05 15:35:36 +0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 ## End to End Data Science Project
+
+MLFLOW_TRACKING_URI=https://dagshub.com/mbasit397/mlproject.mlflow \
+MLFLOW_TRACKING_USERNAME=MuhammadABasit \
+MLFLOW_TRACKING_PASSWORD=e6fac732d16daeea4b82ab08e9d60aa86068511b \
+python script.py
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/mbasit397/mlproject.mlflow
+export MLFLOW_TRACKING_USERNAME=mbasit397
+export MLFLOW_TRACKING_PASSWORD=e6fac732d16daeea4b82ab08e9d60aa86068511b
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -13,33 +13,26 @@ import numpy as np
 
 load_dotenv()
 
-host=os.getenv("host")
-user=os.getenv("user")
-password=os.getenv("password")
-db=os.getenv('db')
-
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv("db")
 
 
 def read_sql_data():
     logging.info("Reading SQL database started")
     try:
-        mydb=pymysql.connect(
-            host=host,
-            user=user,
-            password=password,
-            db=db
-        )
-        logging.info("Connection Established",mydb)
-        df=pd.read_sql_query('Select * from students',mydb)
+        mydb = pymysql.connect(host=host, user=user, password=password, db=db)
+        logging.info("Connection Established", mydb)
+        df = pd.read_sql_query("Select * from students", mydb)
         print(df.head())
 
         return df
 
-
-
     except Exception as ex:
         raise CustomException(ex)
-    
+
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -52,33 +45,34 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-# def evaluate_models(X_train, y_train,X_test,y_test,models,param):
-#     try:
-#         report = {}
 
-#         for i in range(len(list(models))):
-#             model = list(models.values())[i]
-#             para=param[list(models.keys())[i]]
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
 
-#             gs = GridSearchCV(model,para,cv=3)
-#             gs.fit(X_train,y_train)
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
 
-#             model.set_params(**gs.best_params_)
-#             model.fit(X_train,y_train)
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
 
-#             #model.fit(X_train, y_train)  # Train model
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
 
-#             y_train_pred = model.predict(X_train)
+            # model.fit(X_train, y_train)  # Train model
 
-#             y_test_pred = model.predict(X_test)
+            y_train_pred = model.predict(X_train)
 
-#             train_model_score = r2_score(y_train, y_train_pred)
+            y_test_pred = model.predict(X_test)
 
-#             test_model_score = r2_score(y_test, y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
 
-#             report[list(models.keys())[i]] = test_model_score
+            test_model_score = r2_score(y_test, y_test_pred)
 
-#         return report
+            report[list(models.keys())[i]] = test_model_score
 
-#     except Exception as e:
-#         raise CustomException(e, sys)
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
